6235e53845ea6a833d9827e7a05d16058ad8a3aa
Vinicius Mello <viniciusbmello@gmail.com>
2021-02-22 20:41:38 -0300

    Added TypeScript

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main>
+        <h1>Hello NLW</h1>
+      </main>
+        
+    </div>
+  )
+}
