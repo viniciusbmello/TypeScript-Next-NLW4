3bf3f7ffbf2b99b392458c93d766a082ac1db684
Vinicius Mello <viniciusbmello@gmail.com>
2021-02-22 22:46:26 -0300

    Title and Index Style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
 
+import Layout from './style'
+
 const IndexPage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>NLW #4</title>
       </Head>
+      <Layout>
+        <h1>Hello NLW</h1>
+      </Layout>
     </>
   )
 }
---
+++ src/pages/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const Layout = styled.main`
+  margin: 0 auto;
+  padding-top: 3rem;
+`
+
+export default Layout
